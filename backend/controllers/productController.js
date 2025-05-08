7adeb01593309e5e2386b13d7313e689032f91a4
INJAMAM <injamamul937@gmail.com>
2025-05-08 23:37:46 +0600

    tested sql in getProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,14 @@
 import { sql } from "../config/db.js";
 
-export const getProducts = async (req, res) => {};
+export const getProducts = async (req, res) => {
+  try {
+    const products = await sql`SELECT * FROM products`;
+    res.status(200).json({ success: true, data: products });
+  } catch (err) {
+    console.error("Error fetching products:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 export const createProduct = async (req, res) => {};
 export const getProduct = async (req, res) => {};
 export const updateProduct = async (req, res) => {};
