1c8acb203f3d688f393597cf62346f5636222ef4
INJAMAM <injamamul937@gmail.com>
2025-05-10 17:44:42 +0600

    add rate limiting using arcjet

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,41 @@ app.use(express.json());
 app.use(helmet()); // It's a security middleware that helps secure Express apps by setting various HTTP headers
 app.use(morgan("dev")); // Log the requests
 app.use(cors());
+
+//apply arcjet rate limiting
+app.use(async (req, res, next) => {
+  try {
+    const decition = await aj.protect(req, {
+      requested: 1, //specifies that each request consumes 1 token
+    });
+
+    if (decition.isDenied()) {
+      if (decition.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests" });
+      } else if (decition.reason.isBot()) {
+        res.status(403).json({ error: "Bot detected" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    //check for spoofed bots
+    if (
+      decition.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.error("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
