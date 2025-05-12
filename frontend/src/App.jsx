14746026e29babe9fd8077a2135ff5a65a4b22c7
INJAMAM <injamamul937@gmail.com>
2025-05-12 23:58:20 +0600

    Navbar in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import Navbar from "./components/Navbar";
 import React from "react";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500">Hello World</h1>
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
