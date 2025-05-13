e076e19b6ae235e79e00bfd55d11bec5dd3efac2
INJAMAM <injamamul937@gmail.com>
2025-05-13 08:38:50 +0600

    Navbar ongoing

--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { PaletteIcon } from "lucide-react";
+import { THEMES } from "../constants";
 
 function ThemeSelector() {
-  return <div>ThemeSelector</div>;
+  const theme = "forest";
+  return (
+    <div className="dropdown dropdown-end">
+      {/* dropdown trigger */}
+      <button tabIndex={0} className="btn btn-ghost btn-circle">
+        <PaletteIcon className="size-5" />
+      </button>
+      <div
+        tabIndex={0}
+        className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl w-56 border border-base-content/10"
+      >
+        {THEMES.map((themeOption) => (
+          <button
+            key={themeOption.name}
+            className={`w-full px-4 py-3 rounded-xl flex items-center gap-3 transition-colors ${
+              theme === themeOption.name
+                ? "bg-primary/10 text-primary"
+                : "hover:bg-base-content/5"
+            }`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ThemeSelector;
